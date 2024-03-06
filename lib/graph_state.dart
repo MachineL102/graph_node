0d4d1c5ba53cd02ba7397aab5a137b75cafc27ba
zheliu <995220358@qq.com>
2024-03-06 23:48:13 +0800

    [add]

--- lib/graph_state.dart
+++ lib/graph_state.dart
@@ -1,49 +1,81 @@
 import 'dart:ffi';
-import 'package:provider/provider.dart'; 
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:graph_layout/graph_layout.dart';
 import 'package:vector_math/vector_math.dart';
 
-// class NoteNode extends IntegerNode {
-//   NoteNode(super.id);
-//   double _positionX = 0.0;
-//   double _positionY = 0.0;
+// class DirectedEdge extends Edge {
+//   final bool directed;
+//   DirectedEdge(
+//       {required Node left, required Node right, required this.directed, s})
+//       : super(left: left, right: right);
+//   @override
+//   bool operator ==(DirectedEdge other) =>
+//       return this.left.hashCode == other.left.hashCode &&
+//       this.left.hashCode == other.left.hashCode;
 
-//   double get positionX => _positionX;
-//   double get positionY => _positionY;
 // }
+
+enum RelationType {
+  related,
+  parent,
+  child,
+}
+
 class GraphState extends ChangeNotifier {
-    String title = "node title";
+  String title = "node title";
   String mainText = "node main text";
 
   int _current_node_id = 0;
   List<IntegerNode> _graph_nodes = [IntegerNode(0)];
   List<IntegerNode> get graph_nodes => _graph_nodes;
   Set<Edge> _edgeList = Set<Edge>();
+  Set<Edge> get  edgeList => _edgeList;
   NodeLayout _nodeLayout = Map<Node, Vector2>();
-  Map<Node,String> titles = Map<Node,String>();
-  Map<Node,String> mainTexts = Map<Node,String>();
+  Map<Edge, bool> directions = Map<Edge, bool>();
+  Map<Node, String> titles = Map<Node, String>();
+  Map<Node, String> mainTexts = Map<Node, String>();
   GraphState() {
-    _nodeLayout[IntegerNode(0)] = Vector2(1,1);
+    _nodeLayout[IntegerNode(0)] = Vector2(1, 1);
     titles[IntegerNode(0)] = title;
     mainTexts[IntegerNode(0)] = mainText;
   }
 
   Map<Node, Vector2> get nodeLayout => _nodeLayout;
-  
+
   set graph_nodes(List<IntegerNode> value) {
     _graph_nodes = value;
     notifyListeners(); // Notify listeners about the change
   }
 
-  void addRelatedNode(IntegerNode node) {
+  void addRelatedNode(IntegerNode node, RelationType directed) {
     _current_node_id = _current_node_id + 1;
     _graph_nodes.add(IntegerNode(_current_node_id));
 
     titles[_graph_nodes.last] = title;
     mainTexts[_graph_nodes.last] = mainText;
-    _edgeList.add(Edge(left: node, right: _graph_nodes.last));
-    final graph = Graph.fromEdgeList(_edgeList);
+
+    switch (directed) {
+      case RelationType.related:
+    _edgeList.add(
+        Edge(left: node, right: _graph_nodes.last));
+        directions[_edgeList.last] = false;
+        break;
+      case RelationType.parent:
+    _edgeList.add(
+        Edge(left: _graph_nodes.last, right: node));
+        directions[_edgeList.last] = true;
+        break;
+      case RelationType.child:
+    _edgeList.add(
+        Edge(left: node, right: _graph_nodes.last));
+        directions[_edgeList.last] = true;
+        break;
+      default:
+        print('error');
+    }
+
+    final graph = Graph.fromEdgeList(_edgeList.map((edge) => edge as Edge).toSet());
     final layoutAlgorithm = FruchtermanReingold(graph: graph);
     layoutAlgorithm.updateLayoutParameters(
       width: 300,
@@ -52,7 +84,8 @@ class GraphState extends ChangeNotifier {
     );
     layoutAlgorithm.computeLayout();
     for (final nodeLayout in layoutAlgorithm.nodeLayout.entries) {
-      print('the node with identifier ${nodeLayout.key.hashCode} is placed at ${nodeLayout.value}');
+      print(
+          'the node with identifier ${nodeLayout.key.hashCode} is placed at ${nodeLayout.value}');
     }
     _nodeLayout = layoutAlgorithm.nodeLayout;
     notifyListeners(); // Notify listeners about the change
--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,49 @@ import 'line_painter.dart';
 import 'graph_state.dart';
 import 'package:graph_layout/graph_layout.dart';
 import 'package:vector_math/vector_math.dart' as vector_math;
+import 'dart:math';
+import 'dart:ui' as ui;
 
 void main() {
   runApp(MyApp());
+}
+
+class LineSegment {
+  final vector_math.Vector2 start;
+  final vector_math.Vector2 end;
+
+  LineSegment(this.start, this.end);
+}
+
+class Rectangle {
+  final double width;
+  final double height;
+
+  Rectangle(this.width, this.height);
+}
+
+LineSegment calculateAvoidingRectangle(vector_math.Vector2 startPoint,
+    Rectangle startRect, vector_math.Vector2 endPoint, Rectangle endRect) {
+  // 计算起始矩形的左上角和右下角坐标
+  double startX1 = startPoint.x - startRect.width / 2;
+  double startY1 = startPoint.y - startRect.height / 2;
+  double startX2 = startPoint.x + startRect.width / 2;
+  double startY2 = startPoint.y + startRect.height / 2;
+
+  // 计算结束矩形的左上角和右下角坐标
+  double endX1 = endPoint.x - endRect.width / 2;
+  double endY1 = endPoint.y - endRect.height / 2;
+  double endX2 = endPoint.x + endRect.width / 2;
+  double endY2 = endPoint.y + endRect.height / 2;
+
+  // 计算避开矩形区域后的线段的起始位置和终止位置
+  double newX1 = max(startX1, endX1);
+  double newY1 = max(startY1, endY1);
+  double newX2 = min(startX2, endX2);
+  double newY2 = min(startY2, endY2);
+
+  return LineSegment(
+      vector_math.Vector2(newX1, newY1), vector_math.Vector2(newX2, newY2));
 }
 
 class MyApp extends StatelessWidget {
@@ -35,7 +75,6 @@ class GraphView extends StatefulWidget {
 }
 
 class _GraphViewState extends State<GraphView> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,10 +95,33 @@ class _GraphViewState extends State<GraphView> {
                     ),
                   ] +
                   graphState.nodeLayout.entries
-                      .map((MapEntry<Node, vector_math.Vector2> entry) => NodeView(node: entry.key, positionX: entry.value[0], positionY: entry.value[1])
-                      )
-                      .toList().cast<Widget>()
-                      ,
+                      .map((MapEntry<Node, vector_math.Vector2> entry) =>
+                          NodeView(
+                              node: entry.key,
+                              positionX: entry.value[0],
+                              positionY: entry.value[1]))
+                      .toList()
+                      .cast<Widget>() +
+                  graphState.edgeList
+                      .map((edge) {
+                        LineSegment ls = calculateAvoidingRectangle(
+                          graphState.nodeLayout[edge.left] ??
+                              vector_math.Vector2(0, 0),
+                          Rectangle(100.0, 50.0),
+                          graphState.nodeLayout[edge.right] ??
+                              vector_math.Vector2(0, 0),
+                          Rectangle(100.0, 50.0),
+                        );
+
+                        return CustomPaint(
+                          painter: LinePainter(
+                            startPoint: ui.Size(ls.start[0], ls.start[1]),
+                            endPoint: ui.Size(ls.end[0], ls.end[1]),
+                          ),
+                        );
+                      })
+                      .toList()
+                      .cast<Widget>(),
             ),
           ),
         ),
@@ -88,8 +150,12 @@ class _NodeViewState extends State<NodeView> {
   @override
   void initState() {
     super.initState();
-    _titleController = TextEditingController(text: Provider.of<GraphState>(context, listen: false).titles[widget.node]);
-    _mainTextController = TextEditingController(text: Provider.of<GraphState>(context, listen: false).mainTexts[widget.node]);
+    _titleController = TextEditingController(
+        text: Provider.of<GraphState>(context, listen: false)
+            .titles[widget.node]);
+    _mainTextController = TextEditingController(
+        text: Provider.of<GraphState>(context, listen: false)
+            .mainTexts[widget.node]);
   }
 
   @override
@@ -113,13 +179,26 @@ class _NodeViewState extends State<NodeView> {
       ),
       items: [
         PopupMenuItem(
-          child: Text('create Button'),
+          child: Text('create related node'),
           onTap: () {
-            Provider.of<GraphState>(context, listen: false).addRelatedNode(node);
+            Provider.of<GraphState>(context, listen: false)
+                .addRelatedNode(node, RelationType.related);
           },
         ),
-        PopupMenuItem(child: Text('create Text')),
-        // Add more PopupMenuItems as needed
+        PopupMenuItem(
+          child: Text('create parent node'),
+          onTap: () {
+            Provider.of<GraphState>(context, listen: false)
+                .addRelatedNode(node, RelationType.parent);
+          },
+        ),
+        PopupMenuItem(
+          child: Text('create child node'),
+          onTap: () {
+            Provider.of<GraphState>(context, listen: false)
+                .addRelatedNode(node, RelationType.child);
+          },
+        ),
       ],
     );
   }
@@ -150,8 +229,12 @@ class _NodeViewState extends State<NodeView> {
           actions: [
             ElevatedButton(
               onPressed: () {
-                Provider.of<GraphState>(context, listen: false).titles[widget.node] = _titleController.text;
-                Provider.of<GraphState>(context, listen: false).mainTexts[widget.node] = _mainTextController.text;
+                setState(() {
+                  Provider.of<GraphState>(context, listen: false)
+                      .titles[widget.node] = _titleController.text;
+                  Provider.of<GraphState>(context, listen: false)
+                      .mainTexts[widget.node] = _mainTextController.text;
+                });
                 Navigator.of(context2).pop();
               },
               child: Text('Save'),
@@ -171,24 +254,27 @@ class _NodeViewState extends State<NodeView> {
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
-                          left: widget.positionX,
-                          top: widget.positionY,
-                          child: SizedBox(
-                              width: 100,
-                              height: 50,
-                              child: FloatingActionButton(
-                                  child: GestureDetector(
-                                    onSecondaryTap: () {
-                                      _showOptions(context, IntegerNode(widget.node.hashCode));
-                                    },
-                                    child: Text(Provider.of<GraphState>(context, listen: false).titles[widget.node] ?? 'init'),
-                                  ),
-                                  tooltip: 'Increment',
-                                  onPressed: () {
-                                    _createTextEditWindow(context);
-                                  })));
+        left: widget.positionX,
+        top: widget.positionY,
+        child: SizedBox(
+            width: 100,
+            height: 50,
+            child: FloatingActionButton(
+                child: GestureDetector(
+                  onSecondaryTap: () {
+                    _showOptions(context, IntegerNode(widget.node.hashCode));
+                  },
+                  child: Text(Provider.of<GraphState>(context, listen: false)
+                          .titles[widget.node] ??
+                      'init'),
+                ),
+                tooltip: 'Increment',
+                onPressed: () {
+                  _createTextEditWindow(context);
+                })));
   }
 }
